c1c2270c97d8c5fb33439013ccead40999d96aba
Mark Purcell <markpurcell@ie.ibm.com>
2019-10-04 10:40:36 +0000

    Adding the unit test

---
+++ tests/ffl/ffl.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+#author markpurcell@ie.ibm.com
+
+"""
+IBM-Review-Requirement: Art30.3 - DO NOT TRANSFER OR EXCLUSIVELY LICENSE THE FOLLOWING CODE UNTIL 30/11/2025!
+Please note that the following code was developed for the project MUSKETEER in DRL funded by the European Union
+under the Horizon 2020 Program.
+The project started on 01/12/2018 and was completed on 30/11/2021. Thus, in accordance with article 30.3 of the
+Multi-Beneficiary General Model Grant Agreement of the Program, the above limitations are in force until 30/11/2025.
+"""
+
+import logging
+import time
+import json
+import unittest
+import pytest
+import pycloudmessenger.ffl.fflapi as fflapi
+
+
+#Set up logger
+logging.basicConfig(
+    level=logging.INFO,
+    #level=logging.DEBUG,
+    format='%(asctime)s.%(msecs)03d %(levelname)-6s %(name)-16s :: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+LOGGER = logging.getLogger(__package__)
+
+
+
+class FFLTests(unittest.TestCase):
+    #@unittest.skip("temporarily skipping")
+    def test_enum(self):
+        self.assertTrue(fflapi.Notification('task_start') is fflapi.Notification.start)
+
+        with self.assertRaises(ValueError):
+            self.assertTrue(fflapi.Notification('start') is fflapi.Notification.started)
+
+        #Check list searching
+        arr = [fflapi.Notification.start, fflapi.Notification.stop]
+        self.assertTrue(fflapi.Notification('task_start') in arr)
+        self.assertTrue(fflapi.Notification('join') not in arr)
+
+        #Ensure json serializability
+        notify = {'type': fflapi.Notification.join}
+        serialized = json.dumps(notify)
+
+        deserialized = json.loads(serialized)
+        self.assertTrue(fflapi.Notification(deserialized['type']) is fflapi.Notification.join)
