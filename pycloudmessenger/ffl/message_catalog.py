a8ff78f850a60609633d915bbb3ce816928f9065
Mark Purcell <markpurcell@ie.ibm.com>
2019-12-05 16:11:44 +0000

    Support named object upload/download

--- pycloudmessenger/ffl/message_catalog.py
+++ pycloudmessenger/ffl/message_catalog.py
@@ -52,6 +52,16 @@ class MessageCatalog():
             message['serviceRequest']['requestor']['replyTo'] = reply_to
         return message
 
+    def msg_bin_upload_object(self, object_name: str) -> dict:
+        template, args = self._msg_template(service_name='BinService')
+        args.append({'cmd': 'upload_object', 'params': [self.user_name, object_name]})
+        return template
+
+    def msg_bin_download_object(self, object_name: str) -> dict:
+        template, args = self._msg_template(service_name='BinService')
+        args.append({'cmd': 'download_object', 'params': [self.user_name, object_name]})
+        return template
+
     def msg_bin_uploader(self, object_name: str = None) -> dict:
         template, args = self._msg_template(service_name='BinService')
         args.append({'cmd': 'uploader', 'params': [self.user_name, object_name]})
@@ -64,65 +74,65 @@ class MessageCatalog():
 
     def msg_user_create(self, username: str, password: str, organisation: str) -> dict:
         template, args = self._msg_template()
-        args.append({'cmd':'user_create', 'params': [username, password, organisation]})
+        args.append({'cmd': 'user_create', 'params': [username, password, organisation]})
         return template
 
     def msg_user_assignments(self) -> dict:
         template, args = self._msg_template()
-        args.append({'cmd':'user_assignments', 'params': [self.user_name]})
+        args.append({'cmd': 'user_assignments', 'params': [self.user_name]})
         return template
 
     def msg_task_listing(self) -> dict:
         template, args = self._msg_template()
-        args.append({'cmd':'task_listing'})
+        args.append({'cmd': 'task_listing'})
         return template
 
     def msg_task_create(self, task_name: str, topology: str, definition: dict) -> dict:
         template, args = self._msg_template()
-        args.append({'cmd':'task_create', 'params': [task_name, self.user_name, topology, definition]})
+        args.append({'cmd': 'task_create', 'params': [task_name, self.user_name, topology, definition]})
         return template
 
     def msg_task_update(self, task_name: str, topology: str, definition: dict, status: str) -> dict:
         template, args = self._msg_template()
-        args.append({'cmd':'task_update', 'params': [task_name, self.user_name, topology, definition, status]})
+        args.append({'cmd': 'task_update', 'params': [task_name, self.user_name, topology, definition, status]})
         return template
 
     def msg_task_info(self, task_name: str) -> dict:
         template, args = self._msg_template()
-        args.append({'cmd':'task_info', 'params': [task_name, self.user_name]})
+        args.append({'cmd': 'task_info', 'params': [task_name, self.user_name]})
         return template
 
     def msg_task_assignments(self, task_name: str) -> dict:
         template, args = self._msg_template()
-        args.append({'cmd':'task_assignments', 'params': [task_name, self.user_name]})
+        args.append({'cmd': 'task_assignments', 'params': [task_name, self.user_name]})
         return template
 
     def msg_task_join(self, task_name: str) -> dict:
         template, args = self._msg_template()
-        args.append({'cmd':'task_join', 'params': [task_name, self.user_name]})
+        args.append({'cmd': 'task_join', 'params': [task_name, self.user_name]})
         return template
 
     def msg_task_quit(self, task_name: str) -> dict:
         template, args = self._msg_template()
-        args.append({'cmd':'task_quit', 'params': [task_name, self.user_name]})
+        args.append({'cmd': 'task_quit', 'params': [task_name, self.user_name]})
         return template
 
     def msg_task_start(self, task_name: str, model: dict = None) -> dict:
         template, args = self._msg_template()
-        args.append({'cmd':'task_start', 'params': [task_name, self.user_name, model]})
+        args.append({'cmd': 'task_start', 'params': [task_name, self.user_name, model]})
         return template
 
     def msg_task_stop(self, task_name: str) -> dict:
         template, args = self._msg_template()
-        args.append({'cmd':'task_stop', 'params': [task_name, self.user_name]})
+        args.append({'cmd': 'task_stop', 'params': [task_name, self.user_name]})
         return template
 
     def msg_task_assignment_update(self, task_name: str, status: str = None, model: dict = None):
         template, args = self._msg_template()
-        args.append({'cmd':'task_assignment_update', 'params': [task_name, self.user_name, status, model]})
+        args.append({'cmd': 'task_assignment_update', 'params': [task_name, self.user_name, status, model]})
         return template
 
     def msg_task_assignment_info(self, task_name: str):
         template, args = self._msg_template()
-        args.append({'cmd':'task_assignment_info', 'params': [task_name, self.user_name]})
+        args.append({'cmd': 'task_assignment_info', 'params': [task_name, self.user_name]})
         return template
