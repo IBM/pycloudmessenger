9da90b7f1ef04ced614dcb0c96e0cc4b43aba245
Mark Purcell <markpurcell@ie.ibm.com>
2020-01-20 15:24:56 +0000

    Adding unit tests for basic rabbitmq interactions

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
-.PHONY: all test creds depend basic castor ffl
-
+.PHONY: all test creds depend basic castor ffl configure test
+  
 all: test
 
 creds:
@@ -8,10 +8,12 @@ creds:
 	endif
 
 depend:
+	@command -v jq >/dev/null 2>&1 || { echo >&2 "Error - package jq required, but not available."; exit 1; }
 	@command -v python3 >/dev/null 2>&1 || { echo >&2 "Error - package python3 required, but not available."; exit 1; }
+	@command -v docker >/dev/null 2>&1 || { echo >&2 "Error - package docker required, but not available."; exit 1; }
 
-basic: depend creds
-	@test -f $(cert) && python3 -m examples.basic.basic_client --credentials=$(creds) --feed_queue='feed queue' --reply_queue='reply queue' || echo "No certificate"
+basic: depend
+	@test -f $(creds) && python3 -m examples.basic.basic_client --credentials=$(creds) || echo "No certificate"
 
 castor: depend
 	@test -f $(creds) && python3 -m examples.castor.sample_client --credentials=$(creds) || echo "No certificate"
@@ -19,5 +21,11 @@ castor: depend
 ffl: depend
 	@test -f $(creds) && python3 -m examples.ffl.sample --credentials=$(creds) || echo "No certificate"
 
-test:
-	python3 -m pytest tests/ffl/ffl.py -srx -s
+configure: depend
+	./rabbit.sh
+
+clean:
+	-docker rm -f $(shell docker ps -a | grep rabbit_mq | cut -d' ' -f1)
+
+test: configure
+	python3 -m pytest tests/basic/test_basic.py -srx -s --credentials=$(creds)
---
+++ tests/basic/test_basic.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+#author markpurcell@ie.ibm.com
+
+"""
+IBM-Review-Requirement: Art30.3 - DO NOT TRANSFER OR EXCLUSIVELY LICENSE THE FOLLOWING CODE UNTIL 30/11/2025!
+Please note that the following code was developed for the project MUSKETEER in DRL funded by the European Union
+under the Horizon 2020 Program.
+The project started on 01/12/2018 and was completed on 30/11/2021. Thus, in accordance with article 30.3 of the
+Multi-Beneficiary General Model Grant Agreement of the Program, the above limitations are in force until 30/11/2025.
+"""
+
+import argparse
+import logging
+import unittest
+import json
+import pytest
+import pycloudmessenger.rabbitmq as rabbitmq
+
+#Set up logger
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s.%(msecs)03d %(levelname)-6s %(name)s %(thread)d :: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+LOGGER = logging.getLogger(__package__)
+logging.getLogger("pika").setLevel(logging.CRITICAL)
+
+@pytest.mark.usefixtures("credentials","feed_queue","reply_queue")
+#@pytest.mark.usefixtures("credentials")
+class MessengerTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    #@unittest.skip("temporarily skipping")
+    def test_users(self):
+        context = rabbitmq.RabbitContext.from_credentials_file(self.credentials)
+
+        try:
+            with rabbitmq.RabbitClient(context) as client:
+                client.start(publish=rabbitmq.RabbitQueue(context.feeds(), purge=True),
+                             subscribe=rabbitmq.RabbitQueue(context.replies()))
+                message = {'action': 'Outbound', 'payload': 'some data'}
+                LOGGER.info(f'Client sending: {message}')
+                client.publish(json.dumps(message))#, delay=5)
+
+                #Now start the server side and handle the message
+                with rabbitmq.RabbitClient(context) as server:
+                    server.start(subscribe=rabbitmq.RabbitQueue(context.feeds()))
+
+                    recv = server.receive()
+                    LOGGER.info(f'Server received: {recv}')
+
+                    #And send a reply to the client
+                    reply = {'action': 'Inbound', 'reply': 'the reply'}
+                    LOGGER.info(f'Server sending: {reply}')
+                    server.publish(json.dumps(reply), rabbitmq.RabbitQueue(context.replies()))
+
+                #Now catch the reply in the client
+                message = client.receive()
+                LOGGER.info(f"Client received: {message}")
+        except Exception as err:
+            LOGGER.info("Error %r", err)
+            raise err
+
